e1a80bdf34337dcf7486b0914f779195544236a3
alexsandrosr88 <alexsandrosr@msn.com>
2023-09-30 19:52:30 -0300

    Concluído a atividade 5

---
+++ src/atividade5/OrdenacaoPessoas.java
@@ -0,0 +1,35 @@
+package atividade5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    private List<Pessoa> pessoas;
+
+    public OrdenacaoPessoas() {
+        pessoas = new ArrayList<>();
+    }
+
+    public void adcionarPessoa(String nome, int idade, double altura){
+        pessoas.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoas);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoas);
+        pessoasPorAltura.sort(new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+}
+class ComparatorPorAltura implements Comparator<Pessoa> {
+    @Override
+    public int compare(Pessoa p1, Pessoa p2) {
+        return Double.compare(p1.getAltura(), p2.getAltura());
+    }
+}
