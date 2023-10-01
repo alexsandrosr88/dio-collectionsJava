bf0a61938baab99bfa7e982bd3efa415c866b25b
alexsandrosr88 <alexsandrosr@msn.com>
2023-09-30 23:49:01 -0300

    Fix: Incluído os métodos: equals e hashCod indispensáeis no trato de  umalista SET

--- src/atividadesComSet/atividade2/ConjuntoPalavrasUnicas.java
+++ src/atividadesComSet/atividade2/ConjuntoPalavrasUnicas.java
@@ -1,6 +1,7 @@
 package atividadesComSet.atividade2;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class ConjuntoPalavrasUnicas {
@@ -40,6 +41,19 @@ public class ConjuntoPalavrasUnicas {
     @Override
     public String toString() {
         return listaPalavras.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ConjuntoPalavrasUnicas that = (ConjuntoPalavrasUnicas) o;
+        return Objects.equals(listaPalavras, that.listaPalavras);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(listaPalavras);
     }
 
     public static void main(String[] args){
