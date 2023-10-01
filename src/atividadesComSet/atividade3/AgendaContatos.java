503fa4192aa0bdff4c28dd02bbd69db11967ed6b
alexsandrosr88 <alexsandrosr@msn.com>
2023-10-01 00:16:57 -0300

    FEAT: Concluído a atividade 3 da lista SET

---
+++ src/atividadesComSet/atividade3/AgendaContatos.java
@@ -0,0 +1,63 @@
+package atividadesComSet.atividade3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+
+    private Set<Contato> agendaContatos;
+
+    public AgendaContatos() {
+        agendaContatos = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero){
+        agendaContatos.add(new Contato(nome, numero));
+    }
+
+    public void exibirContatos(){
+        for (Contato c : agendaContatos){
+            System.out.println(c);
+        }
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome){
+        Set<Contato> list = new HashSet<>();
+
+        for (Contato c : agendaContatos){
+            if(c.getNome().equalsIgnoreCase(nome))
+                list.add(c);
+        }
+        return list;
+    }
+
+    public void atualizarNumeroContato(String nome, int novoNumero){
+        for (Contato c : agendaContatos){
+            if(c.getNome().equalsIgnoreCase(nome)){
+                c.setNumero(novoNumero);
+            }
+        }
+    }
+    public static void main(String[] args){
+        AgendaContatos agenda = new AgendaContatos();
+
+        agenda.adicionarContato("Fulano1", 455245268);
+        agenda.adicionarContato("Fulano2", 454555548);
+        agenda.adicionarContato("Fulano3", 456826892);
+        agenda.adicionarContato("Fulano4", 456248862);
+        agenda.adicionarContato("Fulano4", 456248862);
+
+        System.out.println();
+        agenda.exibirContatos();
+        System.out.println("\n-------------------------\n");
+
+        System.out.println(agenda.pesquisarPorNome("Fulano4"));
+
+        System.out.println("\n-------------------------\n");
+
+        agenda.atualizarNumeroContato("Fulano4",88884444);
+
+        agenda.exibirContatos();
+
+    }
+}
