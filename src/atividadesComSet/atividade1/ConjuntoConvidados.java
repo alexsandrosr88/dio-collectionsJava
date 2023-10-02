92be194c7997b5559defb7881626e7330f54d87c
alexsandrosr88 <alexsandrosr@msn.com>
2023-10-02 03:28:04 -0300

    FIX: Corrigido o método de remoção da atividade 1

--- src/atividadesComSet/atividade1/ConjuntoConvidados.java
+++ src/atividadesComSet/atividade1/ConjuntoConvidados.java
@@ -16,11 +16,13 @@ public class ConjuntoConvidados {
     }
 
     public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
         for(Convidado d : listaConvidados){
             if(d.getCodigoConvite() == codigoConvite){
-                listaConvidados.remove(d);
+                convidadoParaRemover = d;
             }
         }
+        listaConvidados.remove(convidadoParaRemover);
     }
 
     public int contarConvidados(){
@@ -31,5 +33,28 @@ public class ConjuntoConvidados {
         for (Convidado d : listaConvidados){
             System.out.println(d);
         }
+    }
+    public static void main(String[] args){
+        ConjuntoConvidados convidados = new ConjuntoConvidados();
+
+        convidados.adicionarConvidado("Convidado1", 1);
+        convidados.adicionarConvidado("Convidado2", 2);
+        convidados.adicionarConvidado("Convidado3", 3);
+        convidados.adicionarConvidado("Convidado4", 4);
+        convidados.adicionarConvidado("Convidado5", 5);
+
+        System.out.println("\nLista de convidados");
+        convidados.exibirConvidados();
+
+        System.out.println("\n-----------------------------\n");
+        System.out.println("Removendo convidado por código do convite");
+        convidados.removerConvidadoPorCodigoConvite(2);
+
+        System.out.println("\nLista de convidados, pós alteração");
+        convidados.exibirConvidados();
+
+        System.out.println("\n-----------------------------\n");
+        System.out.println("Quantidade de convidados: "+convidados.contarConvidados());
+
     }
 }
